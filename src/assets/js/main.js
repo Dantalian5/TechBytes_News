1ac059161af51da40faa878d59ece23b1e80876c
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2023-11-26 14:21:34 +0100

    fix layout & ended

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,5 @@
 const hamburgerMenu = document.getElementById("js-navmenu__btn");
+const themeSwitch = document.getElementById("js-themeInput");
 
 document.addEventListener("click", (event) => {
 	// if clicked outside the menu, close the menu, if clicked in the menu, togle
@@ -6,5 +7,14 @@ document.addEventListener("click", (event) => {
 		hamburgerMenu.classList.remove("is-active");
 	} else if (event.target.closest("#js-navmenu__btn")) {
 		hamburgerMenu.classList.toggle("is-active");
+	}
+});
+themeSwitch.addEventListener("change", (event) => {
+	if (event.target.checked) {
+		document.body.classList.add("theme-light");
+		document.body.classList.remove("theme-dark");
+	} else {
+		document.body.classList.add("theme-dark");
+		document.body.classList.remove("theme-light");
 	}
 });
